fd441b931d427fc0c56c61a766b1c1bcbf155a3a
Grulice <zukarukite@gmail.com>
2018-10-14 13:04:28 +0400

    Refactor code with better naming scheme for AES encryption classes

--- Set1/My_AES_ECB.py
+++ Set1/My_AES_ECB.py
@@ -2,7 +2,7 @@ from Crypto.Cipher import AES
 import binascii
 
 
-def AES_ECB_decrypt(key: bytearray, ciphertext: bytearray):
+def decrypt(key: bytearray, ciphertext: bytearray):
     encryption_suite = AES.new(key, AES.MODE_ECB)
     return encryption_suite.decrypt(ciphertext)
 
@@ -14,7 +14,7 @@ def score_likeECB(ciph: bytearray):
 
 # Yellow submarine key decryption
 # encrypted_lines = binascii.a2b_base64(open('./assets/7.txt', mode='r').read())
-# print(AES_ECB_decrypt(bytearray(b'YELLOW SUBMARINE'), encrypted_lines))
+# print(decrypt(bytearray(b'YELLOW SUBMARINE'), encrypted_lines))
 
 
 # Detect AES ECB
